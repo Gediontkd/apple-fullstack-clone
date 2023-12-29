eac0a8ef8e807bc1565ab39075b7774eb39bdde5
Gedion Daniel <gediondaniel321@gmail.com>
2023-12-29 09:46:40 +0100

    some changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,6 @@ import Header from './Components/Header/Header.jsx';
 import Footer from './Components/Footer/Footer.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
